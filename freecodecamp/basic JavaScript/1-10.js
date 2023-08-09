bdc9eb653f0fba8713f59a7010195430263c6963
mabar84 <mabar@tut.by>
2023-08-10 02:12:23 +0300

    9

--- freecodecamp/basic JavaScript/1-10.js
+++ freecodecamp/basic JavaScript/1-10.js
@@ -1,9 +1,2 @@
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+let catName = "Oliver";
+let catSound = "Meow!";
