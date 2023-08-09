972acbc55abed6aef0aa271857ab336c476311a3
mabar84 <mabar@tut.by>
2023-08-10 02:09:02 +0300

    7

--- freecodecamp/basic JavaScript/1-10.js
+++ freecodecamp/basic JavaScript/1-10.js
@@ -1,2 +1,9 @@
-var myFirstName = "ivan";
-var myLastName = "ivanov";
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
