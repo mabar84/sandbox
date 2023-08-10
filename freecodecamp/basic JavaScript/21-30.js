a2be86df05e7e7ee97e2d67e007d98788291589a
mabar84 <mabar@tut.by>
2023-08-10 21:46:57 +0300

    25

--- freecodecamp/basic JavaScript/21-30.js
+++ freecodecamp/basic JavaScript/21-30.js
@@ -1,8 +1,1 @@
-let a = 3;
-let b = 17;
-let c = 12;
-
-// Only change code below this line
-a /= 12;
-b /= 9;
-c /= 7;
+const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
