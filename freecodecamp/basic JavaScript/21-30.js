fd026567a347d23b3b2137e2fe44f7c53af15ab9
mabar84 <mabar@tut.by>
2023-08-10 17:49:23 +0300

    31

---
+++ freecodecamp/basic JavaScript/21-30.js
@@ -0,0 +1,13 @@
+const sum = 10 + 10;
+const difference = 45 - 33;
+const product = 8 * 10;
+const quotient = 66 / 33;
+let myVar = 87;
+myVar++;
+myVar--;
+var myDecimal = 5.7;
+const product2 = 2.0 * 2.5;
+const quotient2 = 4.4 / 2.0; // Change this line
+
+console.clear;
+console.log(-5 % 2);
