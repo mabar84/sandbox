81a506508210707251bf054c64b69e77f779d19e
mabar84 <mabar@tut.by>
2023-08-11 18:58:34 +0300

    38

--- freecodecamp/basic JavaScript/31-40.js
+++ freecodecamp/basic JavaScript/31-40.js
@@ -12,3 +12,12 @@ lastNameLength = lastName.length;
 myStr = "Hello World"; // Change this line
 const thirdLetterOfLastName = lastName[2]; // Change this line
 const lastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
