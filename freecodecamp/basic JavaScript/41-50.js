c7e9b197cd71312fd4240f0fd4b91fd40f647a9f
mabar84 <mabar@tut.by>
2023-08-11 19:19:36 +0300

    51

---
+++ freecodecamp/basic JavaScript/41-50.js
@@ -0,0 +1,32 @@
+// Change code below this line
+const someAdjective = "blabla";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+myStr = "Hello World"; // Change this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+const lastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+
+const teams = [
+  ["Bulls", 23],
+  ["White Sox", 45],
+];
+const myArray = [
+  [1, 2],
+  [3, 4],
+];
