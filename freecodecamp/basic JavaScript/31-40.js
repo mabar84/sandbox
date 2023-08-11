0dd6ba4e41ec142a9226b002a746fd6c024485ca
mabar84 <mabar@tut.by>
2023-08-11 15:39:16 +0300

    33

--- freecodecamp/basic JavaScript/31-40.js
+++ freecodecamp/basic JavaScript/31-40.js
@@ -2,3 +2,10 @@
 const someAdjective = "blabla";
 let myStr = "Learning to code is ";
 myStr += someAdjective;
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
