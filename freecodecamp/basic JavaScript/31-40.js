554c905397498e1b62af49107da702c8e4099726
mabar84 <mabar@tut.by>
2023-08-11 15:35:12 +0300

    31

--- freecodecamp/basic JavaScript/31-40.js
+++ freecodecamp/basic JavaScript/31-40.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const myName = "mabar";
-const myStr = "My name is " + myName + " and I am well!";
+// Change code below this line
+const someAdjective = "blabla";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
