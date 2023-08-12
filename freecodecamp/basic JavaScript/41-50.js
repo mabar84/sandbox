b51bd40599986e29a95b48b101aca5f11147be5a
mabar84 <mabar@tut.by>
2023-08-12 17:48:57 +0300

    43

--- freecodecamp/basic JavaScript/41-50.js
+++ freecodecamp/basic JavaScript/41-50.js
@@ -27,9 +27,10 @@ const teams = [
   ["White Sox", 45],
 ];
 const myArray = [
-  [1, 2],
-  [3, 4],
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
 ];
 
-let myData = myArray[0];
-myArray[0] = 45;
+const myData = myArray[2][1];
