95916c3fd5daa3727e9d895f1ad8b155451d8c45
mabar84 <mabar@tut.by>
2023-08-13 15:58:54 +0300

    58

--- freecodecamp/basic-JavaScript/51-60.js
+++ freecodecamp/basic-JavaScript/51-60.js
@@ -1,13 +1,7 @@
-function nextInLine(arr, item) {
-  arr.push(item);
-  item = arr.shift();
-  return item;
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
 }
-
-// Setup
-let testArr = [1, 2, 3, 4, 5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
