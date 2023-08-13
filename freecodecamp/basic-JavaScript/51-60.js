05ef6b23684f4552f9b334cd4ba9d517146a21fd
mabar84 <mabar@tut.by>
2023-08-13 14:48:40 +0300

    55

--- freecodecamp/basic-JavaScript/51-60.js
+++ freecodecamp/basic-JavaScript/51-60.js
@@ -1,11 +1,16 @@
 // Setup
-const outerWear = "T-Shirt";
+let sum = 0;
 
-function myOutfit() {
-  // Only change code below this line
-  let outerWear = "sweater";
-  // Only change code above this line
-  return outerWear;
+function addThree() {
+  sum = sum + 3;
 }
 
-myOutfit();
+function addFive() {
+  sum = sum + 5;
+}
+// Only change code below this line
+
+// Only change code above this line
+
+addThree();
+addFive();
