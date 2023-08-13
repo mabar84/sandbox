9ff3c934e075b75030505e0354afeaf00130c4c3
mabar84 <mabar@tut.by>
2023-08-13 14:45:48 +0300

    54

--- freecodecamp/basic-JavaScript/51-60.js
+++ freecodecamp/basic-JavaScript/51-60.js
@@ -3,7 +3,7 @@ const outerWear = "T-Shirt";
 
 function myOutfit() {
   // Only change code below this line
-
+  let outerWear = "sweater";
   // Only change code above this line
   return outerWear;
 }
