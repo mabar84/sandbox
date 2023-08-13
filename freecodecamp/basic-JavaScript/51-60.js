b46ccb88a84a990788bdd9bff52f3ef8bbdcdf0a
mabar84 <mabar@tut.by>
2023-08-13 16:03:53 +0300

    60

--- freecodecamp/basic-JavaScript/51-60.js
+++ freecodecamp/basic-JavaScript/51-60.js
@@ -1,6 +1,10 @@
-function trueOrFalse(wasThatTrue) {
-  // Only change code below this line
-  if (wasThatTrue) return "Yes, that was true";
-  return "No, that was false";
-  // Only change code above this line
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
 }
+
+testEqual(10);
