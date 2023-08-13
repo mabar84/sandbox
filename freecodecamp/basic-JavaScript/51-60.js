71452ec5033a8c227f0fa0b0daf2646d34018c50
mabar84 <mabar@tut.by>
2023-08-13 15:16:06 +0300

    56

--- freecodecamp/basic-JavaScript/51-60.js
+++ freecodecamp/basic-JavaScript/51-60.js
@@ -1,9 +1,13 @@
-// Setup
-let processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
+function nextInLine(arr, item) {
+  arr.push(item);
+  item = arr.shift();
+  return item;
 }
 
-// Only change code below this line
-processed = processArg(7);
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
