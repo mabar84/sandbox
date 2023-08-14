5b13080fbaa83a32f9d461a788d8962e77565cdd
mabar84 <mabar@tut.by>
2023-08-15 02:07:25 +0300

    76

--- freecodecamp/basic-JavaScript/71-80.js
+++ freecodecamp/basic-JavaScript/71-80.js
@@ -1,28 +1,14 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-
-function golfScore(par, strokes) {
-  if (strokes === 1) {
-    return names[0];
-  }
-  if (strokes <= par - 2) {
-    return names[1];
-  }
-  if (strokes === par - 1) {
-    return names[2];
-  }
-  if (strokes === par) {
-    return names[3];
-  }
-  if (strokes === par + 1) {
-    return names[4];
-  }
-  if (strokes === par + 2) {
-    return names[5];
-  }
-  if (strokes >= par + 3) {
-    return names[6];
-  }
-  return "Change Me";
+switch (val) {
+  case 1:
+    answer = "alpha";
+    break;
+  case 2:
+    answer = "beta";
+    break;
+  case 3:
+    answer = "gamma";
+    break;
+  case 4:
+    answer = "delta";
+    break;
 }
-
-golfScore(5, 4);
