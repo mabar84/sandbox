4e76808a232ce5e991524e5b1afc0b98d57ce251
mabar84 <mabar@tut.by>
2023-08-14 15:48:40 +0300

    66

--- freecodecamp/basic-JavaScript/61-70.js
+++ freecodecamp/basic-JavaScript/61-70.js
@@ -1,15 +1,15 @@
-function testGreaterThan(val) {
-  if (val > 100) {
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
     // Change this line
-    return "Over 100";
+    return "20 or Over";
   }
 
-  if (val > 10) {
+  if (val >= 10) {
     // Change this line
-    return "Over 10";
+    return "10 or Over";
   }
 
-  return "10 or Under";
+  return "Less than 10";
 }
 
-testGreaterThan(10);
+testGreaterOrEqual(10);
