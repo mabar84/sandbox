14e3a585a96431909e9b17a8a37399a7e334744e
mabar84 <mabar@tut.by>
2023-08-15 02:17:45 +0300

    79

--- freecodecamp/basic-JavaScript/71-80.js
+++ freecodecamp/basic-JavaScript/71-80.js
@@ -1,17 +1,17 @@
 switch (val) {
-  case 1:
-  case 2:
-  case 3:
-    answer = "Low";
+  case "bob":
+    answer = "Marley";
     break;
-  case 4:
-  case 5:
-  case 6:
-    answer = "Mid";
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
     break;
   case 7:
-  case 8:
-  case 9:
-    answer = "High";
+    answer = "Ate Nine";
     break;
 }
