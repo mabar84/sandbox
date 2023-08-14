bd5d371ff2b89b48dae934cecf50129c3d978be4
mabar84 <mabar@tut.by>
2023-08-14 23:44:15 +0300

    72

--- freecodecamp/basic-JavaScript/71-80.js
+++ freecodecamp/basic-JavaScript/71-80.js
@@ -1,15 +1,11 @@
-function testElse(val) {
-  let result = "";
-  // Only change code below this line
-
-  if (val > 5) {
-    result = "Bigger than 5";
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
   } else {
-    result = "5 or Smaller";
+    return "Between 5 and 10";
   }
-
-  // Only change code above this line
-  return result;
 }
 
-testElse(4);
+testElseIf(7);
