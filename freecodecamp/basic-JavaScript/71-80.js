ca3b753de7567f4993e9083c3e71ef02de1f786d
mabar84 <mabar@tut.by>
2023-08-15 00:14:56 +0300

    75

--- freecodecamp/basic-JavaScript/71-80.js
+++ freecodecamp/basic-JavaScript/71-80.js
@@ -1,18 +1,28 @@
-function testSize(num) {
-  // Only change code below this line
-  if (num < 5) {
-    return "Tiny";
-  } else if (num < 10) {
-    return "Small";
-  } else if (num < 15) {
-    return "Medium";
-  } else if (num < 20) {
-    return "Large";
-  } else if (num >= 20) {
-    return "Huge";
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes === 1) {
+    return names[0];
+  }
+  if (strokes <= par - 2) {
+    return names[1];
+  }
+  if (strokes === par - 1) {
+    return names[2];
+  }
+  if (strokes === par) {
+    return names[3];
+  }
+  if (strokes === par + 1) {
+    return names[4];
+  }
+  if (strokes === par + 2) {
+    return names[5];
+  }
+  if (strokes >= par + 3) {
+    return names[6];
   }
   return "Change Me";
-  // Only change code above this line
 }
 
-testSize(7);
+golfScore(5, 4);
