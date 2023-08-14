f4d160fe62233c3501fec9869953825857da99a4
mabar84 <mabar@tut.by>
2023-08-14 23:47:43 +0300

    73

--- freecodecamp/basic-JavaScript/71-80.js
+++ freecodecamp/basic-JavaScript/71-80.js
@@ -1,11 +1,11 @@
-function testElseIf(val) {
-  if (val > 10) {
-    return "Greater than 10";
-  } else if (val < 5) {
-    return "Smaller than 5";
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
   } else {
-    return "Between 5 and 10";
+    return "Greater than or equal to 10";
   }
 }
 
-testElseIf(7);
+orderMyLogic(7);
