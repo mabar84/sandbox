1ff10813482de0881b661976e505ff661a335fce
mabar84 <mabar@tut.by>
2023-08-14 23:42:43 +0300

    71

--- freecodecamp/basic-JavaScript/71-80.js
+++ freecodecamp/basic-JavaScript/71-80.js
@@ -1,12 +1,15 @@
-function testLogicalOr(val) {
+function testElse(val) {
+  let result = "";
   // Only change code below this line
 
-  if (val > 20 || val < 10) {
-    return "Outside";
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
   }
 
   // Only change code above this line
-  return "Inside";
+  return result;
 }
 
-testLogicalOr(15);
+testElse(4);
