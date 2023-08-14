2684cf90cff7bc1ddb79b6e055c353cb6cf5f8a5
mabar84 <mabar@tut.by>
2023-08-14 15:43:42 +0300

    62

--- freecodecamp/basic-JavaScript/61-70.js
+++ freecodecamp/basic-JavaScript/61-70.js
@@ -1,10 +1,10 @@
 // Setup
-function testStrict(val) {
-  if (val === 7) {
+function compareEquality(a, b) {
+  if (a === b) {
     // Change this line
     return "Equal";
   }
   return "Not Equal";
 }
 
-testStrict(10);
+compareEquality(10, "10");
