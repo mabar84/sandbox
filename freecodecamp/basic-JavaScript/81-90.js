f5c5b3140d42fba75622c30685cbe4e3680502e4
mabar84 <mabar@tut.by>
2023-08-16 01:31:17 +0300

    88

--- freecodecamp/basic-JavaScript/81-90.js
+++ freecodecamp/basic-JavaScript/81-90.js
@@ -1,10 +1,8 @@
-// Setup
 const myDog = {
-  name: "Coder",
+  name: "Happy Coder",
   legs: 4,
   tails: 1,
   friends: ["freeCodeCamp Campers"],
 };
 
-// Only change code below this line
-myDog["name"] = "Happy Coder";
+myDog.bark = "woof";
