840ef2bd05261a0aec7f6ca0c4dbc47433c01b07
mabar84 <mabar@tut.by>
2023-08-15 21:51:51 +0300

    81

--- freecodecamp/basic-JavaScript/81-90.js
+++ freecodecamp/basic-JavaScript/81-90.js
@@ -1,7 +1,14 @@
-function isLess(a, b) {
+// Setup
+function abTest(a, b) {
   // Only change code below this line
-  return a < b;
+
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+
   // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-isLess(10, 15);
+abTest(2, 2);
