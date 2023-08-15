a3237afe8b129ccf6fe4645c005df2d0d4f50c80
mabar84 <mabar@tut.by>
2023-08-16 01:27:29 +0300

    86

--- freecodecamp/basic-JavaScript/81-90.js
+++ freecodecamp/basic-JavaScript/81-90.js
@@ -1,10 +1,10 @@
 // Setup
 const testObj = {
-  "an entree": "hamburger",
-  "my side": "veggies",
-  "the drink": "water",
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas",
 };
 
 // Only change code below this line
-const entreeValue = testObj["an entree"]; // Change this line
-const drinkValue = testObj["the drink"]; // Change this line
+const playerNumber = 16; // Change this line
+const player = testObj[playerNumber]; // Change this line
