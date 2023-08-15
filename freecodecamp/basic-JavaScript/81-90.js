2d972beebfe3ebbbc49ebe33f8c455bd7cdadb73
mabar84 <mabar@tut.by>
2023-08-15 22:12:03 +0300

    82

--- freecodecamp/basic-JavaScript/81-90.js
+++ freecodecamp/basic-JavaScript/81-90.js
@@ -1,14 +1,32 @@
-// Setup
-function abTest(a, b) {
-  // Only change code below this line
+let count = 0;
 
-  if (a < 0 || b < 0) {
-    return undefined;
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    default:
+      break;
   }
 
+  return count > 0 ? count + " Bet" : count + " Hold";
   // Only change code above this line
-
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-abTest(2, 2);
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
