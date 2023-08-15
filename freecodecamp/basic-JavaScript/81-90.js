b7798834c7e8f6d6eba85697ee98144d2746d95e
mabar84 <mabar@tut.by>
2023-08-16 01:19:11 +0300

    83

--- freecodecamp/basic-JavaScript/81-90.js
+++ freecodecamp/basic-JavaScript/81-90.js
@@ -1,32 +1,6 @@
-let count = 0;
-
-function cc(card) {
-  // Only change code below this line
-  switch (card) {
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    default:
-      break;
-  }
-
-  return count > 0 ? count + " Bet" : count + " Hold";
-  // Only change code above this line
-}
-
-cc(2);
-cc(3);
-cc(7);
-cc("K");
-cc("A");
+const myDog = {
+  name: "Sharik",
+  legs: 4,
+  tails: 1,
+  friends: ["Bobik"],
+};
