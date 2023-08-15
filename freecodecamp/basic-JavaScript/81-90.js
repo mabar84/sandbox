32bb19c079d1b45678fa073dcc0cea95fb1951a7
mabar84 <mabar@tut.by>
2023-08-16 01:23:47 +0300

    85

--- freecodecamp/basic-JavaScript/81-90.js
+++ freecodecamp/basic-JavaScript/81-90.js
@@ -1,10 +1,10 @@
 // Setup
 const testObj = {
-  hat: "ballcap",
-  shirt: "jersey",
-  shoes: "cleats",
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water",
 };
 
 // Only change code below this line
-const hatValue = testObj.hat; // Change this line
-const shirtValue = testObj.shirt; // Change this line
+const entreeValue = testObj["an entree"]; // Change this line
+const drinkValue = testObj["the drink"]; // Change this line
