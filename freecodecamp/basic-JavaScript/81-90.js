54b50476ffc7b9436e35de2b7ef2cf7b56393f98
mabar84 <mabar@tut.by>
2023-08-16 01:39:12 +0300

    90

--- freecodecamp/basic-JavaScript/81-90.js
+++ freecodecamp/basic-JavaScript/81-90.js
@@ -1,11 +1,20 @@
 // Setup
-const myDog = {
-  name: "Happy Coder",
-  legs: 4,
-  tails: 1,
-  friends: ["freeCodeCamp Campers"],
-  bark: "woof",
-};
+function phoneticLookup(val) {
+  let result = "";
 
-// Only change code below this line
-delete myDog["tails"];
+  // Only change code below this line
+  const lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+  result = lookup[val];
+  console.log(result);
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
