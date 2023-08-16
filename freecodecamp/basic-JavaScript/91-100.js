48288d7a712608aeddcceec9907bc7a9c251e9fa
mabar84 <mabar@tut.by>
2023-08-17 00:35:02 +0300

    94

--- freecodecamp/basic-JavaScript/91-100.js
+++ freecodecamp/basic-JavaScript/91-100.js
@@ -1,13 +1,12 @@
-const myStorage = {
-  car: {
-    inside: {
-      "glove box": "maps",
-      "passenger seat": "crumbs",
-    },
-    outside: {
-      trunk: "jack",
-    },
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
   },
-};
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
 
-const gloveBoxContents = myStorage.car.inside["glove box"];
+const secondTree = myPlants[1].list[1];
