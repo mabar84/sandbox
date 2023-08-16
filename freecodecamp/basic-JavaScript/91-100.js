2b5cffac8f92a08e6ce8616983e9cb1a96862b66
mabar84 <mabar@tut.by>
2023-08-17 01:20:29 +0300

    95

--- freecodecamp/basic-JavaScript/91-100.js
+++ freecodecamp/basic-JavaScript/91-100.js
@@ -1,12 +1,10 @@
-const myPlants = [
-  {
-    type: "flowers",
-    list: ["rose", "tulip", "dandelion"],
-  },
-  {
-    type: "trees",
-    list: ["fir", "pine", "birch"],
-  },
-];
+// Setup
+const myArray = [];
 
-const secondTree = myPlants[1].list[1];
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+console.log(myArray);
+// Only change code below this line
