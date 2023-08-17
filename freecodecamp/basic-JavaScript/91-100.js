222aad46f5ee19e72a7d5c4fbdd175d17484419d
mabar84 <mabar@tut.by>
2023-08-18 01:46:07 +0300

    96

--- freecodecamp/basic-JavaScript/91-100.js
+++ freecodecamp/basic-JavaScript/91-100.js
@@ -1,10 +1,8 @@
 // Setup
 const myArray = [];
 
-let i = 5;
-while (i >= 0) {
+for (let i = 1; i <= 5; i++) {
   myArray.push(i);
-  i--;
 }
 console.log(myArray);
 // Only change code below this line
