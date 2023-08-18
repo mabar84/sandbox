c04da8e379dbbe835e2ad42bf3b2e89ca920e466
mabar84 <mabar@tut.by>
2023-08-19 01:55:12 +0300

    99

--- freecodecamp/basic-JavaScript/91-100.js
+++ freecodecamp/basic-JavaScript/91-100.js
@@ -1,8 +1,38 @@
 // Setup
-const myArray = [];
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
 
-for (let i = 9; i > 0; i -= 2) {
-  myArray.push(i);
-}
-console.log(myArray);
 // Only change code below this line
+function updateRecords(records, id, prop, value) {
+  const album = records[id];
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop === "tracks") {
+    if (album["tracks"]) {
+      album["tracks"].push(value);
+    } else {
+      album["tracks"] = [value];
+    }
+  } else album[prop] = value;
+
+  return records;
+}
+
+updateRecords(recordCollection, 5439, "artist", "ABBA");
