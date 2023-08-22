b6e20e44a591113d1426b3f21ccf20881106c04f
mabar84 <mabar@tut.by>
2023-08-22 11:21:49 +0300

    102

--- freecodecamp/basic-JavaScript/101-113.js
+++ freecodecamp/basic-JavaScript/101-113.js
@@ -1,18 +1,10 @@
-function multiplyAll(arr) {
-  let product = 1;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      product *= arr[i][j];
-      console.log(product);
-    }
-  }
-  //   console.log(product);
-  return product;
-}
+// Setup
+const myArray = [];
+let i = 10;
 
-multiplyAll([
-  [1, 2],
-  [3, 4],
-  [5, 6, 7],
-]);
-// console.log(multiplyAll(arr));
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
+
+console.log(myArray);
