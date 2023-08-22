71003305f48c50fd20378d37cbd63a8ed780d28d
mabar84 <mabar@tut.by>
2023-08-23 02:29:13 +0300

    103

--- freecodecamp/basic-JavaScript/101-113.js
+++ freecodecamp/basic-JavaScript/101-113.js
@@ -1,9 +1,50 @@
-function sum(arr, n) {
-  // Only change code below this line
-  if (n <= 0) {
-    return 0;
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  let foundedContacts = contacts.filter((el) => el["firstName"] === name);
+
+  if (!foundedContacts.length) {
+    return "No such contact";
   } else {
-    return sum(arr, n - 1) + arr[n - 1];
+    for (let i = 0; i < foundedContacts.length; i++) {
+      if (foundedContacts[i]["firstName"] === name) {
+        if (foundedContacts[i].hasOwnProperty(prop)) {
+          return foundedContacts[i][prop];
+        } else {
+          return "No such property";
+        }
+      }
+    }
   }
-  // Only change code above this line
 }
+
+// lookUpProfile("Akira", "likes");
+// lookUpProfile("Kristian", "lastName");
+console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Kristian", "lastName"));
