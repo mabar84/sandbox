8ed186ecfa4be33d79d80724ff62c2950bce673b
mabar84 <mabar@tut.by>
2023-08-24 18:33:07 +0300

    105

--- freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
+++ freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
@@ -1,50 +1,9 @@
-// Setup
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+function randomFraction() {
+  // Only change code below this line
 
-function lookUpProfile(name, prop) {
-  let foundedContacts = contacts.filter((el) => el["firstName"] === name);
+  let a = Math.random();
 
-  if (!foundedContacts.length) {
-    return "No such contact";
-  } else {
-    for (let i = 0; i < foundedContacts.length; i++) {
-      if (foundedContacts[i]["firstName"] === name) {
-        if (foundedContacts[i].hasOwnProperty(prop)) {
-          return foundedContacts[i][prop];
-        } else {
-          return "No such property";
-        }
-      }
-    }
-  }
+  return a;
+
+  // Only change code above this line
 }
-
-// lookUpProfile("Akira", "likes");
-// lookUpProfile("Kristian", "lastName");
-console.log(lookUpProfile("Akira", "likes"));
-console.log(lookUpProfile("Kristian", "lastName"));
