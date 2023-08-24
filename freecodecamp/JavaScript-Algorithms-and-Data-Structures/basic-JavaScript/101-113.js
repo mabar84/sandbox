7b4b9742ff38ec2e5891e249da2a642da5324278
mabar84 <mabar@tut.by>
2023-08-24 18:36:02 +0300

    106

--- freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
+++ freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
@@ -1,9 +1,4 @@
-function randomFraction() {
-  // Only change code below this line
-
-  let a = Math.random();
-
-  return a;
-
-  // Only change code above this line
+function randomWholeNum() {
+  return Math.floor(Math.random() * 2);
 }
+console.log(randomWholeNum());
