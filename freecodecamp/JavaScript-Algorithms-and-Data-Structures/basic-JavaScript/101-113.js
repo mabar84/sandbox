a4a758b451da4c5a0acdfa2a0c38dfbc2911dd65
mabar84 <mabar@tut.by>
2023-09-02 22:51:44 +0300

    113

--- freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
+++ freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
@@ -1,14 +1,21 @@
-// Only change code below this line
+// function countdown(n) {
+//   if (n < 1) {
+//     return [];
+//   } else {
+//     const countArr = countdown(n - 1);
+//     countArr.unshift(n);
+//     return countArr;
+//   }
+// }
 
-function countdown(n) {
-  if (n < 1) {
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
     return [];
   } else {
-    const countArr = countdown(n - 1);
-    countArr.unshift(n);
+    const countArr = rangeOfNumbers(startNum, endNum - 1);
+    countArr.push(endNum);
+    // console.log(countArr);
     return countArr;
   }
 }
-
-console.log(countdown(5));
-// Only change code above this line
+console.log(rangeOfNumbers(6, 6));
