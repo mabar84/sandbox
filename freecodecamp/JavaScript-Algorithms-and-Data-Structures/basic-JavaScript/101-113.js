2856c13f7e731ab8760381414e34bfda6bcbd76a
mabar84 <mabar@tut.by>
2023-09-02 23:27:42 +0300

    trainig js 18

--- freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
+++ freecodecamp/JavaScript-Algorithms-and-Data-Structures/basic-JavaScript/101-113.js
@@ -1,20 +1,9 @@
-// function countdown(n) {
-//   if (n < 1) {
-//     return [];
-//   } else {
-//     const countArr = countdown(n - 1);
-//     countArr.unshift(n);
-//     return countArr;
-//   }
-// }
-
 function rangeOfNumbers(startNum, endNum) {
   if (endNum < startNum) {
     return [];
   } else {
     const countArr = rangeOfNumbers(startNum, endNum - 1);
     countArr.push(endNum);
-    // console.log(countArr);
     return countArr;
   }
 }
