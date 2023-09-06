d1cbfa204f665755db35aa21b8c5c50484fcbbbe
mabar84 <mabar@tut.by>
2023-09-06 18:08:43 +0300

    28

--- freecodecamp/JavaScript-Algorithms-and-Data-Structures/es6/11-29.js
+++ freecodecamp/JavaScript-Algorithms-and-Data-Structures/es6/11-29.js
@@ -1,10 +1,9 @@
 const makeServerRequest = new Promise((resolve, reject) => {
-  // responseFromServer represents a response from a server
-  let responseFromServer;
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
 
-  if (responseFromServer) {
-    resolve("We got the data");
-  } else {
-    reject("Data not received");
-  }
+  makeServerRequest.then((result) => {
+    console.log(result);
+  });
 });
+console.log(12);
